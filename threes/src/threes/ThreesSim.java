e646872a9eedfa5d5c5999821741ac581e9905d7
Josh Kaplan <jkaplan@salesforce.com>
2017-11-15 19:54:27 -0800

    rearranged main to merge w master

--- threes/src/threes/ThreesSim.java
+++ threes/src/threes/ThreesSim.java
@@ -12,117 +12,73 @@ import threes.mongo.*;
 import threes.analysis.*;
 
 public class ThreesSim {
-    public static void main(String[] args) {
-    	Mongo m = new Mongo();
-/*
-    	String s;
-    	HashSet<String> deads = new HashSet<String>();
-    	List<WriteModel<Document>> ld;
-    	QueryUtil qu = new QueryUtil(m);
+	public static void main(String[] args) {
+		Mongo m = null;
+		//uncomment to write things
+		//m = new Mongo();
+		
+  		// String s = "133035270483354a";
+  		// String s = "132103142471253a";
+  		// String s = "213103020267579a";
+  		// String s = "000313340475369a";
+String s = "15334420456348ab";
+  		// String s = "15415200456348ab";
+		// String s = "00000000800089ab";
 
-    	s = "244556363261789a";
     	Board b = new Board(s);
-//    	System.out.println(BoardUtil.zeroOneRowBoardScore(b));
-//    	System.out.println(BoardUtil.countColsMove(b));
-//    	System.out.println(BoardUtil.countRowsMove(b));
-/*
-    	int count = 0;
-    	for (int i = 1; i <= 1; i++) {
-        	for (int j = 1; j <= 1; j++) {
-	        	s = "1" + i + j +"111111111789a";
-	    		System.out.println(new Date() + " " + s);
-		    	ld = threes.analysis.EndGame.ones(s, 1, 6);
-		    	if (ld.size() > 0) 
-		    		m.insertBulk("zeroOneRowBoards",ld);
-		    	ld = null;
-//		    	deads = threes.analysis.EndGame.ones(s, 1, 6);
-//		    	count += deads.size();
-//	    		System.out.println(new Date() + " adding " + deads.size() + " total " + count);
-//		    	deads = null;
-        	}
-    	}
-		System.out.println(new Date() + " done");
-//		System.out.println(new Date() + " " + deads.size());
-*/    	   	
-//   		String s = "133035270483354a";
-//   		String s = "132103142471253a";
-//   		String s = "213103020267579a";
-//   		String s = "000313340475369a";
-//   		String s = "15334420456348ab";
-//   		String s = "15415200456348ab";
-		String s = "00000000800089ab";
 
-		Board b = new Board(s);
+		ThreesSim.simulate(b, m);
+//		bigMergeInserts(m);
 
-		// BoardMaker bm = new BoardMaker(b, 0, 2);
-		// bm.setFinalX(2);
-		// bm.setFinalY(3);
-    	// Date start = new Date();
-		// long i = 0;
-		// Board bb = bm.getNext();
-		// while (bb != null) {
-		// 	i++;
-		// 	bb = bm.getNext();
-		// 	if (bb == null) {
-		// 		System.out.println("MAHOOLEEHOO!!! " + i);
-		// 		break;
-		// 	}
-		// }
-		// bm.getCurrent().printBoard();
-    	// long elapsedSeconds = (new Date().getTime() - start.getTime())/1000;
-		// System.out.println(new Date() + " Finished. Time: " + elapsedSeconds/60 + ":" +elapsedSeconds%60);
-		bigMergeInserts(m);
+		if (m != null) {
+			m.closeOut();
+		}
+	}
 
+	private static void simulate(Board b, Mongo m) {
+		Aggregator a = new Aggregator(b);
+		
+		if (m != null) {
+			a.setMongo(m);
+		}
+		int count = 0;
+		long score = 0;
+		long moves = 0;
+		long highScore = 0;
+		long highMoves = 0;
+				
+		Date start = new Date();
 
-		/*
-     	Aggregator a = new Aggregator(b);
-//    	a.setMongo(m);
-    	int count = 0;
-    	long score = 0;
-    	long moves = 0;
-    	long highScore = 0;
-    	long highMoves = 0;
-    	
-    	Date start = new Date();
+		for (int i = 0; i < 1; i++) {
+			System.out.println(new Date() + " LOOP #" +(i+1));
+			a.runLoop(5000,20);
+			
+			count += a.getCount();
+			score += a.getScore();
+			moves += a.getMoves();
+			
+			highScore = Math.max(highScore, a.getBestScore());
+			highMoves = Math.max(highMoves, a.getBestMoves());
 
-    	for (int i = 0; i < 1; i++) {
-    		System.out.println(new Date() + " LOOP #" +(i+1));
-	    	a.runLoop(5000,20);
-	    	
-	    	count += a.getCount();
-	    	score += a.getScore();
-	    	moves += a.getMoves();
-	    	
-	    	highScore = Math.max(highScore, a.getBestScore());
-	    	highMoves = Math.max(highMoves, a.getBestMoves());
-
-//	    	QueryUtil q = new QueryUtil(m);
-//	    	MongoCursor<Document> cursor = q.runIDQuery(a.getKeyStrings());
-//	    	HashMap<String,MergedResults> existingMongo = MergedResults.doTheThing(cursor);
-//	    	a.mergeAndMongo(existingMongo);
-//	WAS:	    	a.mergeAndMongo(q.doTheThing(a.getKeyStrings()));
-//	    	m.insertRecords(a.mrMongos);
-	
-	    	a.cleanup();
-    	}
-    	long elapsedSeconds = (new Date().getTime() - start.getTime())/1000;
+			if (m != null) {
+				QueryUtil q = new QueryUtil(m);
+				MongoCursor<Document> cursor = q.runIDQuery(a.getKeyStrings());
+				HashMap<String,MergedResults> existingMongo = MergedResults.doTheThing(cursor);
+				a.mergeAndMongo(existingMongo);
+//			WAS:	    	a.mergeAndMongo(q.doTheThing(a.getKeyStrings()));
+				m.insertRecords(a.mrMongos);
+			}
+			
+			a.cleanup();
+		}
+		long elapsedSeconds = (new Date().getTime() - start.getTime())/1000;
 		System.out.println(new Date() + " Finished. Time: " + elapsedSeconds/60 + ":" +elapsedSeconds%60);
 		System.out.println(new Date() + " Starting score: " + b.boardScore());
 		System.out.println(new Date() + " runs: " + count + " score: " + score/count + " moves: " + moves*1.0/count);
 		System.out.println(new Date() + " best: " + highScore + ", " + highMoves + " moves");
-
-/*    	
- 
-    	Simulator s = new Simulator(b);
-    	s.setMongo(m);
-    	s.runSim(true);
-    	System.out.println("Score: " + s.getScore() + " moves: " + s.getMoves());
-//
-    	Analyzer az = new Analyzer();
-    	az.findDeadMoves(m);
-/*/
-    	m.closeOut();
+				
 	}
+
 	private static void bigMergeInserts(Mongo m) {
 		String s = "00000000800089ab";		
 //		String s = "00000800080009ab";		
@@ -235,3 +191,50 @@ public class ThreesSim {
 		
 	}
 }
+
+
+
+		/*
+    	String s;
+    	HashSet<String> deads = new HashSet<String>();
+    	List<WriteModel<Document>> ld;
+    	QueryUtil qu = new QueryUtil(m);
+
+    	s = "244556363261789a";
+    	Board b = new Board(s);
+//    	System.out.println(BoardUtil.zeroOneRowBoardScore(b));
+//    	System.out.println(BoardUtil.countColsMove(b));
+//    	System.out.println(BoardUtil.countRowsMove(b));
+/*
+    	int count = 0;
+    	for (int i = 1; i <= 1; i++) {
+        	for (int j = 1; j <= 1; j++) {
+	        	s = "1" + i + j +"111111111789a";
+	    		System.out.println(new Date() + " " + s);
+		    	ld = threes.analysis.EndGame.ones(s, 1, 6);
+		    	if (ld.size() > 0) 
+		    		m.insertBulk("zeroOneRowBoards",ld);
+		    	ld = null;
+//		    	deads = threes.analysis.EndGame.ones(s, 1, 6);
+//		    	count += deads.size();
+//	    		System.out.println(new Date() + " adding " + deads.size() + " total " + count);
+//		    	deads = null;
+        	}
+    	}
+		System.out.println(new Date() + " done");
+//		System.out.println(new Date() + " " + deads.size());
+*/    	   	
+
+/*/    	
+    	b.printBoard();
+    	System.out.println(BoardUtil.isDeadBoard(b));
+
+		
+    	Simulator s = new Simulator(b);
+    	s.setMongo(m);
+    	s.runSim(true);
+    	System.out.println("Score: " + s.getScore() + " moves: " + s.getMoves());
+//
+    	Analyzer az = new Analyzer();
+    	az.findDeadMoves(m);
+/*/
